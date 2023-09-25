f91d40a8f3462515a1a08b7fd7c5ed554a2d272d
gusgus98 <82188302+gusgus98@users.noreply.github.com>
2023-09-25 16:10:51 -0400

    Fixed Error message for no Name input

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -18,8 +18,15 @@ class MainActivity : AppCompatActivity() {
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
 
-        
+
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
+            // Check if the name EditText is empty
+            if (findViewById<EditText>(R.id.nameEditText).text.isEmpty()) {
+                // Display an error message
+                displayTextView.text = "Please enter your name."
+            }
+            else
+            // Otherwise, display the greeting message
             displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
         }
 
